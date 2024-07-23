9792f08a661aee1bf360c7ab1f2f8920b847416f
Elie VISCHEL <elie.vischel@ardentis.ch>
2024-07-23 17:13:23 +0200

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elievischel/laravel-dicom',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e0e86af390a9c1ca2f970b877f8ea2ffe3b73a8c',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elievischel/laravel-dicom' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e0e86af390a9c1ca2f970b877f8ea2ffe3b73a8c',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
